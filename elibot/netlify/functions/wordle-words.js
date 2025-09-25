6fb49fc717e50eca7c1b5cd655224ceeee8405a0
SafsalGaming <79075952+SafsalGaming@users.noreply.github.com>
2025-09-25 13:53:29 +0300

    Create wordle-words.js

---
+++ elibot/netlify/functions/wordle-words.js
@@ -0,0 +1,8 @@
+// netlify/functions/wordle-words.js
+// רשימת תשובות אפשריות. מחליטים מתוכה מילה יומית לכל משתמש.
+// אפשר להרחיב כרצונך. אין "allowed", כל מילה שנרשמת עם 5 אותיות חוקית.
+export const WORDLE_ANSWERS = [
+  "apple","brave","chair","doubt","eagle","flame","grape","heart","input","joker",
+  "knock","lemon","magic","noble","ocean","pride","queen","river","sound","tiger",
+  "unity","vivid","world","xenon","yacht","zebra"
+];
